070d7526d1a5340b71f0c3a0948a017a63641624
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2017-03-08 01:09:39 -0500

    update boot script of final system

--- prepare-remote-script.sh
+++ prepare-remote-script.sh
@@ -7,6 +7,6 @@ REMOTE_FOLDER="remote"
 mkimage -A arm -O linux -T script -C none -n "U-Boot boot script" -d ${REMOTE_FOLDER}/boot.txt ${REMOTE_FOLDER}/boot.scr
 
 
-#echo "Kill me when no longer needed"
-#cd remote
-#python2 -m SimpleHTTPServer
+echo "Kill me when no longer needed"
+cd remote
+python2 -m SimpleHTTPServer
