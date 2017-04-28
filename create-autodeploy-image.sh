1f1f3707963541bca78887366a5151bc5b20cac3
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2017-04-28 13:37:00 -0400

    make filename not version dependent

--- create-autodeploy-image.sh
+++ create-autodeploy-image.sh
@@ -22,8 +22,8 @@ partprobe
 echo "install boot loader"
 losetup /dev/loop0 ${OUTPUT_FOLDER}/autodeploy.img
 # mainline u-boot
-dd if=u-boot-bin/SPL-201701 of=/dev/loop0 bs=1K seek=1
-dd if=u-boot-bin/u-boot-201701.img of=/dev/loop0 bs=1K seek=69
+dd if=u-boot-bin/SPL of=/dev/loop0 bs=1K seek=1
+dd if=u-boot-bin/u-boot.img of=/dev/loop0 bs=1K seek=69
 losetup -d /dev/loop0
 
 echo "format image"
@@ -35,8 +35,8 @@ mkdir ${AUTODEPLOY_MOUNT}
 mount /dev/loop0 ${AUTODEPLOY_MOUNT}
 
 echo "copy files"
-cp kernel-bin/imx6q-cubox-i-4.9.7.dtb ${AUTODEPLOY_MOUNT}/imx6q-cubox-i.dtb
-cp kernel-bin/zImage-4.9.7 ${AUTODEPLOY_MOUNT}/zImage
+cp kernel-bin/imx6q-cubox-i.dtb ${AUTODEPLOY_MOUNT}/imx6q-cubox-i.dtb
+cp kernel-bin/zImage ${AUTODEPLOY_MOUNT}/zImage
 mkimage -A arm -O linux -T script -C none -n "U-Boot boot script" -d config/boot.txt ${AUTODEPLOY_MOUNT}/boot.scr
 cp ${OUTPUT_FOLDER}/autodeploy-initramfs.cpio.gz ${AUTODEPLOY_MOUNT}/
 cp ${CONFIG_FOLDER}/autodeploy-script-source ${AUTODEPLOY_MOUNT}/
