e204c2db172578110313fef4e43701e752f9f7e6
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2017-04-28 13:36:39 -0400

    update copy-recursive script

--- copy-recursive-ll.sh
+++ copy-recursive-ll.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
+set -eux
 
 # this script can be replaced by lddtree one pax-utils went stable
 
 if [ "$#" -ne 2 ] ; then
     echo "need executable and dest"
+    exit 1
 fi
 
 EXECUTABLE=$1
@@ -12,10 +13,14 @@ DEST_DIR=$2
 
 if [ -e "$EXECUTABLE" ] && [ -f "$EXECUTABLE" ] && [ -e "$DEST_DIR" ] && [ -d "$DEST_DIR" ]; then
     if ! [ -e "$DEST_DIR$EXECUTABLE" ] ; then
-        ldd "$EXECUTABLE" | sed 's/[^\/]*\/\(.*\) .*/\/\1/' | \
+        ldd "$EXECUTABLE" | sed 's/linux-vdso\.so.*//'  | sed 's/[^\/]*\/\(.*\) .*/\/\1/' | \
         while read library
         do
             echo "library: " $library
+	    if [ "$library" == "" ] ; then
+		    continue
+	    fi
+
             bash $0 $library $DEST_DIR
         done
         echo "copying executable $EXECUTABLE"
