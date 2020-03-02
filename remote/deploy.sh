99feb2f1f94b89cdf79dc1558c7fc37b76eb7fde
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-02 10:02:54 +0000

    preparing new tag v0.45

--- remote/deploy.sh
+++ remote/deploy.sh
@@ -4,7 +4,7 @@ set -eux
 
 # there are no temporary downloads, the space is limited all files should be streamed directly in place
 
-TAG="v0.44"
+TAG="v0.45"
 
 echo "SETUP userland"
 ARCH=$(uname -m)
