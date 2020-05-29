993c22848faa1e3b485f0b830bdd7995b6029a2e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-29 14:34:28 +0000

    preparing new tag v0.49

--- create-tag.sh
+++ create-tag.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # this is the version that will be used for the next tag
-TAG="v0.49"
+TAG="v0.50"
 DESC="Version bump to latest kernel"
 
 OWNER=stefan-langenmaier
--- remote/deploy.sh
+++ remote/deploy.sh
@@ -4,7 +4,7 @@ set -eux
 
 # there are no temporary downloads, the space is limited all files should be streamed directly in place
 
-TAG="v0.48"
+TAG="v0.49"
 
 echo "SETUP userland"
 ARCH=$(uname -m)
